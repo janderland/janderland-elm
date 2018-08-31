ef027c61d084df4b619a15e360ad70dad3e93ee4
Jon Anderson <jon.david.anderson@gmail.com>
2018-08-30 18:10:09 -0700

    Moved the loadcfg.sh script into jander.sh.

--- scripts/jander.sh
+++ scripts/jander.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
+
+CONFIG=config.env
+
+
+if [ -e $CONFIG ]
+then
+    # Load configuration variables into current scope
+    source $CONFIG
+
+    # Export all the variable so they are available in
+    # child processes.
+    export $(cut -d= -f1 $CONFIG)
+else
+    echo "Config file \"${CONFIG}\" is missing"
+    echo
+    exit 1
+fi
+
+
 TASKS=( "link"  "generate" "compile" )
-
-
-source scripts/loadcfg.sh
 
 
 for TASK in "${TASKS[@]}"
