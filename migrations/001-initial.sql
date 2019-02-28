58ba38dc5c4bba4dda6c235af2402821ff794e79
Jon Anderson <jon.david.anderson@gmail.com>
2019-02-28 00:08:29 -0800

    Added indexes. Recreated db.

--- migrations/001-initial.sql
+++ migrations/001-initial.sql
@@ -5,6 +5,10 @@ create table Contents(
   title text  not null
 );
 
+create unique index Contents_title_index on Contents(title);
+
+
+
 create table Versions(
   id        integer  primary key,
   date      datetime not null,
@@ -14,23 +18,44 @@ create table Versions(
   foreign key(contentId) references Contents(id)
 );
 
+create index Versions_contentId_index on Versions(contentId);
+
+
+
 create table Tags(
   id   integer primary key,
   name text    unique
 );
+
+create unique index Tags_name_index on Tags(name);
+
+
 
 create table VersionsTags(
   id        integer primary key,
   versionId integer not null,
   tagId     integer not null,
 
-  foreign key(versionId) references Versions(id)
+  foreign key(versionId) references Versions(id),
   foreign key(tagId)     references Tags(id)
 );
 
+create index VersionsTags_versionId_index on VersionsTags(versionId);
+create index VersionsTags_tagId_index     on VersionsTags(tagId);
+
+
+
 -- Down
 
+drop index Contents_title_index;
 drop table Contents;
+
+drop index Versions_contentId_index;
 drop table Versions;
+
+drop index Tags_name_index;
 drop table Tags;
+
+drop index VersionsTags_versionId_index;
+drop index VersionsTags_tagId_index;
 drop table VersionsTags;
